15a2f24a98274d6a24c724f608ce3dee4be00458
Roman Yakovlev <tensegrity666@eicloud.com>
2019-12-23 02:22:54 +0300

    Fixes name of const

--- coctails.js
+++ coctails.js
@@ -1,15 +1,15 @@
-const drinks = {
+const DRINKS = {
   coctailOne: ['vodka', 'rum', 'beer', 'milk',],
   coctailTwo: ['martini', 'beer', 'ice'],
   coctailThree: ['vodka', 'beer', 'ice',],
   coctailFour: ['milk', 'ice',],
   coctailFive: ['vodka', 'rum', 'martini',],
-}
+};
 
 const ingridientFinder = (...ingridient) => {
   const result = [];
-  for (const item in drinks) {
-    const coctail = drinks[item];
+  for (const item in DRINKS) {
+    const coctail = DRINKS[item];
     coctail.filter( i => {
       if (ingridient.includes(i)) {
         result.push(item);
